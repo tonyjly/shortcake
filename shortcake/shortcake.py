83bec66182dbf4e79b01bbcf564910c07995f288
Tony Ly <tonyjly@gmail.com>
2022-02-21 05:15:45 +0000

    create class ShortLink and implement generate short link feature

--- shortcake/shortcake.py
+++ shortcake/shortcake.py
@@ -6,8 +6,10 @@ class ShortLink:
     """Generate a short link."""
 
 
-    def __init__(self, long_url):
-        self.long_url = long_url
+    def __init__(self, long_link):
+        self.long_link = long_link
+        self.random_string = ""
+        self.short_link = ""
 
 
     def generate_string(self, size=8):
@@ -16,6 +18,12 @@ class ShortLink:
         self.random_string = ''.join(random.choice(chars) for i in range(size))
 
 
+    def generate_short_link(self):
+        """Generate a Short Link."""
+        self.generate_string()
+        self.short_link = 'https://www.dell.com/' + self.random_string
+
+
 link1 = ShortLink("https://www.dell.com")
-link1.generate_string()
-print(link1.random_string)
+link1.generate_short_link()
+print(link1.short_link)
