663c8d04e4ad552ad2ec99655f5f1d07635a791b
Tony Ly <tonyjly@gmail.com>
2022-02-25 13:56:45 +0000

    refactor: migrate create_link function to crud

--- shortcake/crud.py
+++ shortcake/crud.py
@@ -1,9 +1,33 @@
 from sqlalchemy.orm import Session
+from pydantic import HttpUrl
 import string
 import random
 import models
 import schemas
 
+def create_link(link: HttpUrl, db: Session, new_short_link: str):
+    """Create a new link and add to db
+    """
+    # Check if target link already exists
+    db_link = db.query(models.Link).filter(models.Link.long_link==link.long_link).first()
+
+    if db_link is not None:
+        db_link = db.query(models.ShortLink).filter(models.ShortLink.id==db_link.short_link_id).first()
+        return {
+            "Status": "Link already exists.",
+            "Original link": db_link.long_link,
+            "Short link": "http://127.0.0.1:8000/" + new_short_link.short_link_path
+        }
+
+    new_link = models.Link(
+        long_link = link.long_link,
+        short_link_id = new_short_link.id
+    )
+
+    db.add(new_link)
+    db.commit()
+    db.refresh(new_link)
+    return new_link
 
 def create_short_link(db: Session, link: schemas.ShortLink):
     """Create and add a new short link
--- shortcake/main.py
+++ shortcake/main.py
@@ -1,12 +1,11 @@
-from typing import List # Optional,
+from typing import List
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from starlette.responses import RedirectResponse
-import uvicorn
-
-import models, schemas, crud
 from db import SessionLocal, Base, engine
+import models, schemas, crud
+import uvicorn
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -20,7 +19,6 @@ app.add_middleware(
     allow_credentials=True,
 )
 
-
 def get_db():
     """Database dependency
     This dependency creates a new SessionLocal used for a single
@@ -32,49 +30,45 @@ def get_db():
     finally:
         db.close()
 
-
 @app.get("/", status_code=status.HTTP_200_OK)
 def main(db: Session = Depends(get_db)):
     """Show all links upon entry
     """
     return db.query(models.Link).all()
 
-
 @app.get("/links", response_model=List[schemas.Link])
 def read_links(db: Session = Depends(get_db)):
     links = db.query(models.Link).all()
     return links
-
 
 @app.get("/short-links", response_model=List[schemas.ShortLink])
 def read_short_links(db: Session = Depends(get_db)):
     short_links = db.query(models.ShortLink).all()
     return short_links
 
-
-@app.post("/add-link", response_model=schemas.Link)
-def create_link(link: schemas.Link, db: Session = Depends(get_db)):
+@app.post("/add-link")
+def add_link(link: schemas.Link, db: Session = Depends(get_db)):
     """Create a new link and add to db
     """
     # Check if target link already exists
     db_link = db.query(models.Link).filter(models.Link.long_link==link.long_link).first()
 
     if db_link is not None:
-        # raise HTTPException(status_code=400, detail="Link already exists.")
         db_short_link = db.query(models.ShortLink).filter(models.ShortLink.id==db_link.short_link_id).first()
-        return RedirectResponse(url=db_short_link.short_link, status_code=302)
+        return {
+            "Status": "Link already exists.",
+            "Original link": db_link.long_link,
+            "Short link": "http://127.0.0.1:8000/" + db_short_link.short_link_path
+        }
 
     new_short_link = crud.create_short_link(db=db, link=link)
+    new_link = crud.create_link(db=db, link=link, new_short_link=new_short_link)
 
-    new_link = models.Link(
-        long_link = link.long_link,
-        short_link_id = new_short_link.id
-    )
-
-    db.add(new_link)
-    db.commit()
-    db.refresh(new_link)
-    return new_link
+    return {
+        "Status": "Link successfully processed.",
+        "Original link": new_link.long_link,
+        "Short link": "http://127.0.0.1:8000/" + new_short_link.short_link_path
+    }
 
 @app.get("/{short_link_path}", response_model=schemas.ShortLink)
 def get_long_link(short_link_path: str, db: Session = Depends(get_db)):
